a1977c797981077b9b0f1caa4dfe992f075c261d
petr00l <54990231+petr00l@users.noreply.github.com>
2024-09-02 17:47:28 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_TestTask_TFLEX
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ShapeFactory _shapeFactory;
+        Point _lastFigureMargin = new ();
+        Grid _currentParametersGrid;
+        float _prevMaxRowHeight;
+
+        public MainWindow()
+        {
+            _shapeFactory = new ();
+            InitializeComponent();
+            InitializeComboBox();
+        }
+
+        private void InitializeComboBox()
+        {
+            comboBox.ItemsSource = new[]
+            {
+                nameof(Figures.Circle),
+                nameof(Figures.Ellipse),
+                nameof(Figures.Rectangle),
+                nameof(Figures.RegularPolygon),
+                nameof(Figures.RegularTriangle),
+                nameof(Figures.Square),
+            };
+        }
+
+        private void DrawButtonClick(object sender, RoutedEventArgs e)
+        {
+            Figures.Figure? targetFigure = null;
+
+            try
+            {
+                targetFigure = comboBox.SelectedItem switch
+                {
+                    nameof(Figures.Circle) => new Figures.Circle(
+                        float.Parse(textBoxCircleRadius.Text)),
+                    nameof(Figures.Ellipse) => new Figures.Ellipse(
+                        float.Parse(textBoxEllipseA.Text),
+                        float.Parse(textBoxEllipseB.Text)),
+                    nameof(Figures.Rectangle) => new Figures.Rectangle(
+                        float.Parse(textBoxRectangleWidth.Text),
+                        float.Parse(textBoxRectangleHeight.Text)),
+                    nameof(Figures.RegularPolygon) => new Figures.RegularPolygon(
+                        float.Parse(textBoxPolygonR.Text),
+                        uint.Parse(textBoxPolygonN.Text)),
+                    nameof(Figures.RegularTriangle) => new Figures.RegularTriangle(
+                        float.Parse(textBoxTriangleR.Text)),
+                    nameof(Figures.Square) => new Figures.Square(
+                        float.Parse(textBoxSquareA.Text)),
+                    _ => null
+                };
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Invalid input!");
+                return;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Exception [{ex.GetType().FullName}]: {ex.Message}");
+                return;
+            }
+
+            if (targetFigure is null)
+            {
+                MessageBox.Show("Invalid combobox selection!");
+                return;
+            }
+
+            (Shape s, Label l) = _shapeFactory.CreateShape(targetFigure);
+
+            l.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            float maxWidth = MathF.Max((float)s.Width, (float)l.DesiredSize.Width);
+
+            double currentHeight = s.Height + l.DesiredSize.Height;
+
+            if (_lastFigureMargin.X + maxWidth > canvas.ActualWidth)
+            {
+                _lastFigureMargin.X = 0;
+                _lastFigureMargin.Y += _prevMaxRowHeight;
+
+                _prevMaxRowHeight = (float)currentHeight;
+
+                double requiredHeight = currentHeight + _lastFigureMargin.Y;
+
+                if (requiredHeight > canvas.ActualHeight)
+                {
+                    canvas.Height = requiredHeight;
+                    scrollViewer.ScrollToEnd();
+                }
+            }
+            else
+            {
+                if (currentHeight > _prevMaxRowHeight)
+                    _prevMaxRowHeight = (float)currentHeight;
+            }
+
+            s.Margin = new Thickness(_lastFigureMargin.X, _lastFigureMargin.Y, 0, 0);
+            l.Margin = new Thickness(_lastFigureMargin.X, _lastFigureMargin.Y + s.Height, 0, 0);
+
+            _lastFigureMargin.X += maxWidth;
+
+            canvas.Children.Add(s);
+            canvas.Children.Add(l);
+        }
+
+        private void ComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var targetGrid = comboBox.SelectedItem switch
+            {
+                nameof(Figures.Circle) => circleParametersGrid,
+                nameof(Figures.Ellipse) => ellipseParametersGrid,
+                nameof(Figures.Rectangle) => rectangleParametersGrid,
+                nameof(Figures.RegularPolygon) => polygonParametersGrid,
+                nameof(Figures.RegularTriangle) => triangleParametersGrid,
+                nameof(Figures.Square) => squareParametersGrid,
+                _ => throw new Exception("Invalid combobox selected item")
+            };
+
+            targetGrid.Visibility = Visibility.Visible;
+
+            if (_currentParametersGrid is { })
+                _currentParametersGrid.Visibility = Visibility.Hidden;   
+
+            _currentParametersGrid = targetGrid;
+        }
+
+        private void ClearButtonClick(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+        }
+    }
+}
---
+++ ShapeFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Wpf_TestTask_TFLEX
+{
+    internal class ShapeFactory
+    {
+        public (System.Windows.Shapes.Shape, Label) CreateShape(Figures.Figure figure)
+        {
+            System.Windows.Shapes.Shape shape = figure.FigureType switch
+            {
+                nameof(Figures.Rectangle) or
+                nameof(Figures.Square) => new System.Windows.Shapes.Rectangle()
+                {
+                    Width = (figure as Figures.Rectangle).Width,
+                    Height = (figure as Figures.Rectangle).Height,
+                },
+                nameof(Figures.Ellipse) or
+                nameof(Figures.Circle) => new System.Windows.Shapes.Ellipse()
+                {
+                    Width = 2 * (figure as Figures.Ellipse).A,
+                    Height = 2 * (figure as Figures.Ellipse).B
+                },
+                nameof(Figures.RegularPolygon) or
+                nameof(Figures.RegularTriangle) => new System.Windows.Shapes.Polygon()
+                {
+                    Width = 2 * (figure as Figures.RegularPolygon).R,
+                    Height = 2 * (figure as Figures.RegularPolygon).R,
+                    Points = new (
+                        (figure as Figures.RegularPolygon).Points
+                            .Select(p => new Point(p.x, p.y)))
+                },
+                _ => throw new NotImplementedException($"ShapeFactory.CreateChape: Passed figure type [{figure.FigureType}] not implemented")
+            };
+
+            shape.Stroke = Brushes.Black;
+            shape.StrokeThickness = 2;
+
+            Label label = new()
+            {
+                Content = $"Fig: {figure.FigureType}\nP = {figure.Perimeter}\nS = {figure.Square}"
+            };
+
+            return (shape, label);
+        }
+    }
+}
